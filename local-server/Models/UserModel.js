07e09b96b9a6c3b13ad38782297d083bf4000260
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-03-26 16:58:21 +0700

    separate models to separated files in /Models

---
+++ local-server/Models/UserModel.js
@@ -0,0 +1,47 @@
+import { Sequelize, DataTypes } from "sequelize";
+import sequelize from "../database";
+
+const user = sequelize.define('User', 
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email:{
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate:{
+                isEmail: true,
+                notEmpty: true
+            }
+        },
+        passwordHash:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notEmpty: true,
+                len: [8, 100],
+            }
+        },
+        role:{
+            type: DataTypes.ENUM('user', 'admin'),
+            defaultValue: 'user',
+            allowNull: false
+        },
+    }, {
+        tableName: 'users',
+        underscored: true,
+        timestamps: true,
+        updatedAt: false
+    }
+)
+
+export {user}
--- local-server/database.js
+++ local-server/database.js
@@ -1,5 +1,9 @@
-import { Sequelize, DataTypes, INTEGER } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 import ENV from './env'
+import {user} from "./Models/UserModel"
+import {tagRoads, tagVersions} from "./Models/TagModel"
+
+//database.js hanya untuk koneksi dengan db dan fungsi CRUD
 
 const sequelize = new Sequelize({
     dialect: 'postgres',
@@ -10,115 +14,4 @@ const sequelize = new Sequelize({
     port: 5432
 })
 
-const user = sequelize.define('User', 
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        email:{
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false,
-            validate:{
-                isEmail: true,
-                notEmpty: true
-            }
-        },
-        passwordHash:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                notEmpty: true,
-                len: [8, 100],
-            }
-        },
-        role:{
-            type: DataTypes.ENUM('user', 'admin'),
-            defaultValue: 'user',
-            allowNull: false
-        },
-    }, {
-        tableName: 'users',
-        underscored: true,
-        timestamps: true,
-        updatedAt: false
-    }
-)
-
-const tagRoads = sequelize.define('TagRoad',
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        latitude:{
-            type: DataTypes.DECIMAL(10,8),
-            allowNull: false
-        },
-        longitude:{
-            type: DataTypes.DECIMAL(11,8),
-            allowNull: false
-        },
-        isHidden:{
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        activeVersionId:{
-            type: DataTypes.INTEGER
-        },
-    },{
-        tableName: 'tag_roads',
-        underscored: true,
-        timestamps: true,
-        updatedAt: false
-    }
-)
-
-const tagVersions = sequelize.define('TagVersion',
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        tagRoadId:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        userId:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        conditionStatus:{
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description:{
-            type: DataTypes.TEXT
-        },
-        reliabilityScore:{
-            type: DataTypes.DOUBLE,
-            defaultValue: 0
-        },
-        isPriorityValidated:{
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-    },{
-        tableName: 'tag_versions',
-        underscored: true,
-        timestamps: true,
-        updatedAt: false
-    }
-)
+export {sequelize}
