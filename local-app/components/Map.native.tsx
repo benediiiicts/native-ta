ab1bfaa138af65886e03fca615eb2a776a5884b8
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-03-29 20:24:44 +0700

    untested: connecting frontend and backend for login feature

--- local-app/components/Map.native.tsx
+++ local-app/components/Map.native.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import MapView, { Polyline, UrlTile, type Region } from "react-native-maps";
 
-let API_KEY = process.env.API_KEY;
+let API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 let map_path = `https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.png?key=${API_KEY}`;
 
 async function fetchOverpass(s: number, w: number, n: number, e: number) {
