3ee37b91e398fd2a8bca080c7466a287a92fffe6
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-03-31 17:15:48 +0700

    fixing login and register display in home, adding logout

--- local-app/components/Map.web.tsx
+++ local-app/components/Map.web.tsx
@@ -4,6 +4,7 @@ import { lazy, Suspense } from "react";
 const MapLoader = lazy(() => import('./MapLoader.web'));
 
 function Map({ active }: { active: boolean }){
+
     return(
         <View style={{ flex: 1 }}>
             <Suspense fallback={<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Text>Loading Map...</Text></View>}>
