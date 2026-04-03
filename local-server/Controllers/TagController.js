71ba93f57cbf8c226e68218fec129be137dd43cb
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-04-03 21:48:54 +0700

    adding images model and service to handle save image

---
+++ local-server/Controllers/TagController.js
@@ -0,0 +1,15 @@
+import {createTagRoad, getTagRoad, deleteTagRoad, updateTagRoad, createTagVersion, getTagVersion, deleteTagVersion, updateTagVersion} from '../Services/TagServices.js'
+
+async function addNewTagRoad(req, res){
+    try{
+        const {userId, latitude, longitude, status, description, images, forceCreate} = req.body
+
+    }
+    catch(error){
+
+    }
+}
+
+async function addNewTagVersion(req, res){
+
+}
