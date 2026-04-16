0053039add6b918b94baba45bf92e9ac321a70f4
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-04-16 12:14:28 +0700

    fixing and finalizing comments feature

--- local-server/Controllers/TagController.js
+++ local-server/Controllers/TagController.js
@@ -3,9 +3,7 @@ import {
     getAllTags, 
     getTagDetail, 
     createTagVersion, 
-    voteTagVersion, 
-    commentTagVersion, 
-    loadComment,} from '../Services/TagService.js'
+    voteTagVersion} from '../Services/TagService.js'
 import {saveImages} from '../Services/MediaService.js'
 
 async function addNewTagRoad(req, res){
@@ -99,47 +97,10 @@ async function handleVote(req, res){
     }
 }
 
-async function handleComment(req, res){
-    try{
-        const userId = req.user.id
-        const tagId = req.params.id
-        const {content} = req.body
-        const images = req.files || []
-        let commentResult = await commentTagVersion(userId, tagId, content, images)
-        return res.status(commentResult.status).json({
-            data: commentResult.data,
-            status: commentResult.status,
-            message: commentResult.message
-        })
-    }
-    catch(error){
-        console.error(`Error while posting comment: ${error}`)
-        return res.status(500).json({message: `Internal error while posting comment`})
-    }
-}
-
-async function handleLoadComment(req, res){
-    try{
-        const tagId = req.params.id
-        let commentResult = await loadComment(tagId)
-        return res.status(commentResult.status).json({
-            data: commentResult.data,
-            status: commentResult.status,
-            message: commentResult.message
-        })
-    }
-    catch(error){
-        console.error(`Error while fetching comments ${error}`)
-        return res.status(500).json({message: `Internal error while fetching comments`})
-    }
-}
-
 export {
     addNewTagRoad,
     addNewTagVersion,
     fetchAllTags,
     fetchTagDetails,
     handleVote,
-    handleComment,
-    handleLoadComment
 }
