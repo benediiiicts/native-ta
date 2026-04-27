1e7bd22b48d6290b63426205acb04315f8f863b2
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-04-27 09:11:31 +0700

    adding backend function for admin manage users feature

--- local-server/Controllers/AdminController.js
+++ local-server/Controllers/AdminController.js
@@ -1,4 +1,4 @@
-import { manageTags } from "../Services/AdminService.js";
+import { manageTags, manageUserStatus, fetchAllUsers } from "../Services/AdminService.js";
 
 async function manageTagAdmin(req, res){
     try{
@@ -21,6 +21,59 @@ async function manageTagAdmin(req, res){
     }
 }
 
+async function manageUserAdmin(req, res){
+    try{
+        const userId = req.user.id
+        const userRole = req.user.role
+
+        if(userRole !== 'admin' || !userId){
+            return res.status(401).json({
+                message: "Unauthorized user, feature prohibited"
+            })
+        }
+
+        const targetUserId = req.params.id
+        const { action, durationDays, adminNotes } = req.body
+
+        const result = await manageUserStatus(targetUserId, action, durationDays, adminNotes)
+
+        return res.status(result.status).json({
+            status: result.status,
+            message: result.message
+        })
+    }
+    catch(error){
+        console.error(`Error while managing user: ${error}`)
+        return res.status(500).json({
+            message: "Internal server error while managing user"
+        })
+    }
+}
+
+async function getUsersAdmin(req, res){
+    try{
+        const userId = req.user.id
+        const userRole = req.user.role
+
+        if(userRole !== 'admin' || !userId){
+            return res.status(403).json({
+                message: "Unauthorized user, feature forbidden"
+            })
+        }
+
+        const searchQuery = req.query.search || ''
+        const result = await fetchAllUsers(searchQuery)
+
+        return res.status(result.status).json(result)
+    }
+    catch(error){
+        console.error(`Error while get users: ${error}`)
+        return res.status(500).json({message: "Server error while fetching users"});
+    }
+}
+
 export {
-    manageTagAdmin
+    manageTagAdmin,
+    manageUserAdmin,
+    getUsersAdmin
 }
