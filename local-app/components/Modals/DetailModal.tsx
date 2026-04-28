47c2bbbf6c1e76a7472545450222254966b43ae8
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-04-28 22:21:02 +0700

    fixing functional flow for user report detail, adding image preview

--- local-app/components/Modals/DetailModal.tsx
+++ local-app/components/Modals/DetailModal.tsx
@@ -367,7 +367,10 @@ function DetailModal({ visible, onClose, tagId, currentUserId, userRole, onTagUp
 
     function renderDetails(){
         if (isLoading || !tagData) 
-            return <Text style={styles.loadingText}>Loading...</Text>;
+            return <Text style={styles.loadingText}>Loading...</Text>
+
+        if (!isLoading && !tagData) 
+            return <Text style={styles.loadingText}>Data tidak ditemukan atau sudah dihapus.</Text>
 
         const activeVersion = tagData.activeVersion
         const images = activeVersion?.images || [];
