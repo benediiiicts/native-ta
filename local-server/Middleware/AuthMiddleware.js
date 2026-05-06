5031190ea285fa1d150b54275488929311f7726d
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2026-05-06 22:58:53 +0700

    fixing user auth error

--- local-server/Middleware/AuthMiddleware.js
+++ local-server/Middleware/AuthMiddleware.js
@@ -10,7 +10,7 @@ function authenticateToken (req, res, next) {
         });
     }
     try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
